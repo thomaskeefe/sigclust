50847c6fa72750dd2b03650f08043466740ef63c
Thomas Keefe <thom.keefe@gmail.com>
2020-05-03 16:19:15 -0400

    Improve tests
    Use a non-square data matrix for some tests, which help detect issues of mismatching dimensions

--- test_sigclust.py
+++ test_sigclust.py
@@ -6,37 +6,33 @@ from unittest import TestCase
 class TestUtilityFunctions(TestCase):
     "Test the deterministic functions that SigClust uses"
     def setUp(self):
-        # Test data is the 4 points on the unit square
-        self.test_data = np.array([[-1, 1], [-1, -1], [1, 1], [1, -1]])
+        self.test_data = np.array([[-1, 1], [-1, 0], [-1, -1], [1, 1], [1, 0], [1, -1]])
 
     def test_compute_sum_of_square_distances_to_mean(self):
-        # Initialize points on the unit square in R^2.
-        # The mean is (0,0), so each point has distance
-        # sqrt(2), so the sum of square distances is
-        # 4*2 = 8
+        # On paper we can work out that the mean of test_data is the origin
+        # and the sum of square distances is 4*2 + 2 = 10.
         sum_squares = sigclust.compute_sum_of_square_distances_to_mean(self.test_data)
-        self.assertAlmostEqual(sum_squares, 8, places=8)
+        self.assertAlmostEqual(sum_squares, 10, places=8)
 
     def test_compute_cluster_index_with_numeric_labels(self):
-        # Initialize points on the unit square in R^2.
-        # Points on the left side are class 1, points
-        # on the right side are class 2.
+        # Set labels so that test points with x coordinate of -1 are class 1
+        # and x coordinate of 1 are class 2.
         # The within class sum of squares for each is 2.
-        # The total sum of squares is 4*2=8.
-        # So the cluster index is 1/2.
-        labels = np.array([1, 1, 2, 2])
+        # The total sum of squares is 4*2 + 2 = 10.
+        # So the cluster index is 4/10
+        labels = np.array([1, 1, 1, 2, 2, 2])
         ci = sigclust.compute_cluster_index(self.test_data, labels)
-        self.assertAlmostEqual(ci, 1.0/2, places=8)
+        self.assertAlmostEqual(ci, 4.0/10, places=8)
 
     def test_compute_cluster_index_with_string_labels(self):
         # Make sure string labels are ok
-        ci = sigclust.compute_cluster_index(self.test_data, ['a', 'a', 'b', 'b'])
-        self.assertAlmostEqual(ci, 1.0/2, places=8)
+        ci = sigclust.compute_cluster_index(self.test_data, ['a', 'a', 'a', 'b', 'b', 'b'])
+        self.assertAlmostEqual(ci, 4.0/10, places=8)
 
     def test_compute_cluster_index_with_boolean_labels(self):
         # It would be very ugly to use boolean labels but it should still work
-        ci = sigclust.compute_cluster_index(self.test_data, [True, True, False, False])
-        self.assertAlmostEqual(ci, 1.0/2, places=8)
+        ci = sigclust.compute_cluster_index(self.test_data, [True, True, True, False, False, False])
+        self.assertAlmostEqual(ci, 4.0/10, places=8)
 
     def test_compute_cluster_index_with_bad_labels(self):
         "Make sure errors raise for bad input cluster labelings"
